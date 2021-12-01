634ed75800de5d6c70aa14ab9e70b28e7c7ac86a
luksdev <lucasarinisio@hotmail.com>
2021-12-01 19:46:06 -0300

    Adição das opcoes na listagem de livros DELETE E EDIT

--- scripts/allbooks.js
+++ scripts/allbooks.js
@@ -14,6 +14,9 @@ function main(){
     for(allbooks of Object.keys(livros['livros'])){
         console.log(allbooks)
         for(let i = 0; i <= Object.keys(livros['livros']).length; i++){
+
+            /**Adiciona nome do liro */
+
             var elemento_pai= document.getElementById("body-table")
 
             var celula = document.createElement('tr')
@@ -26,12 +29,39 @@ function main(){
             celula.appendChild(linha)
             elemento_pai.appendChild(celula)
 
+            /**Adiciona nome do autor */
+
             var linha2 = document.createElement('td')
 
             celula.appendChild(linha2)
             elemento_pai.appendChild(celula)
 
             linha2.textContent = `${livros['livros'][i]['autor']}`
+
+            /**Adiciona opções DELETE, EDIT,  */
+
+            var linha3 = document.createElement('td')
+            addUl = document.createElement('ul')
+            addUl.className = "list-inline m-0";
+
+            addLi2 = document.createElement('li')
+            addLi2.innerHTML = ('<button class="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i></button>')
+
+            addLi3 = document.createElement('li')
+            addLi3.innerHTML = ('<button class="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"></i></button>')
+
+            addUl.appendChild(addLi2)
+            addUl.appendChild(addLi3)
+            linha3.appendChild(addUl)
+            celula.appendChild(linha3)
+            elemento_pai.appendChild(celula)
+
+            addLi2.className = ("list-inline-item");
+            addLi3.className = ("list-inline-item");
+
+
+
+            
             
         }
     }
